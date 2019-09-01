db137f87b6c6e37988425b51855713f3a713cfcc
= <=>
2019-09-01 13:09:47 +0800

    feat: v0.5

--- SZUCourseRogue.js
+++ SZUCourseRogue.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         SZUCourseRogue
 // @namespace    https://github.com/zzilch/SZUCourseRogue
-// @version      0.1
+// @version      0.5
 // @description  A script for SZU course selecting.
 // @author       zzilch
 // @match        http://210.39.12.30/xsxkapp/sys/xsxkapp/*default/*
 // @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
 // ==/UserScript==
 
 (function () {
@@ -13,35 +16,66 @@
     // infos
     var studentInfo = JSON.parse(sessionStorage['studentInfo'])
     console.log(`
-    使用说明：（暂不支持状态保存和带实验课的课程）
-    showData():显示当前待抢课程
+    使用说明：（暂不支持带实验课的课程）
+    showDataList():显示当前待抢课程列表
     setDeltaTime(time):设置抢课间隔(ms)
-    setData(tcId[,tcType]):设置待抢课程，tcId为教学班ID，tcType为课程类型，默认自动获取
+    includeData(tcId):查询课程是否再列表中
+    addData(tcId[,tcType]):设置待抢课程，tcId为教学班ID，tcType为课程类型，默认自动获取
+    removeData(tcId):删除课程
+    clearData():清空课程列表
+    syncData():将课程列表保存到本地
     getTeachingClassCapacity(tcId):查询课程剩余位置
-    rogueRepeatedly():开始抢课
+    startRogue():开始抢课
     stopRogue():停止抢课
     `);
 
     // data
-    var data = null;
-    var deltaTime = 1000;
+    var dataList = GM_getValue('dataList', [])
+    var deltaTime = GM_getValue('deltaTime', 1000);
     var stop = true;
 
-    function showData(){
-        console.log('tcId: '+data[0]+', tcType: '+data[1]);
+    function showDataList(){
+        for(let i=0;i<dataList.length;i++)
+            console.log('tcId: '+dataList[i][0]+', tcType: '+dataList[i][1]);
     }
-    unsafeWindow.showData = showData
+    unsafeWindow.showDataList = showDataList
 
     function setDeltaTime(time) {
         deltaTime = Math.max(time, 0);
+        GM_setValue('deltaTime', deltaTime);
     }
     unsafeWindow.setDeltaTime = setDeltaTime;
 
-    function setData(tcId, tcType) {
-        if (tcType == null) tcType = sessionStorage.getItem("teachingClassType");
-        data=[tcId,tcType];
+    function includeData(tcId) {
+        return dataList.find(x => x[0] === tcId);
     }
-    unsafeWindow.setData = setData;
+    unsafeWindow.includeData = includeData;
+
+    function clearData() {
+        GM_deleteValue('dataList');
+        dataList = [];
+    }
+    unsafeWindow.clearData = clearData;
+
+    function addData(tcId, tcType) {
+        if (includeData(tcId)) return;
+        if (tcType == null) tcType = sessionStorage.getItem("teachingClassType");
+        dataList.push([tcId,tcType]);
+        GM_setValue('dataList', dataList);
+    }
+    unsafeWindow.addData = addData;
+
+    function removeData(tcId) {
+        if (!includeData(tcId)) return;
+        dataList = dataList.filter(x => x[0] !== tcId);
+        GM_setValue('dataList', dataList);
+    }
+    unsafeWindow.removeData = removeData;
+
+    function syncData() {
+        GM_setValue('dataList', dataList);
+    }
+    unsafeWindow.syncData = syncData;
 
     // hooks
 
@@ -64,7 +98,7 @@
     }
     unsafeWindow.getTeachingClassCapacity = getTeachingClassCapacity;
 
-    function rogueRepeatedly() {
+    function rogueRepeatedly(data) {
         async function rogueOnce(data) {
             if (stop) {
                 clearInterval(interval);
@@ -86,10 +120,17 @@
                 console.log(data[0] + ': fail, capacity=' + capacity.toString());
             }
         }
-        stop = false;
         var interval = setInterval(rogueOnce, deltaTime, data);
     }
-    unsafeWindow.rogueRepeatedly = rogueRepeatedly;
+
+    function startRogue() {
+        if (stop == false) return;
+        stop = false;
+        for (let i = 0; i < dataList.length; i++) {
+            rogueRepeatedly(dataList[i]);
+        }
+    }
+    unsafeWindow.startRogue = startRogue;
 
     function stopRogue() {
         stop = true;
